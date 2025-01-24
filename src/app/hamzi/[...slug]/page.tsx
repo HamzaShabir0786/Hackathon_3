b41d2a66f8f31ace097d5fbd00cbdacd141a674e
Hamza Shabir <hamzii2205@gmail.com>
2025-01-24 22:06:32 +0500

    delete hamzi folder from this project

--- src/app/hamzi/[...slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function hamzy({ params }: { params: { slug: string } }) {
-  return (
-    <div>
-      <div>I am hamzi {params.slug} </div>
-    </div>
-  );
-}
--- src/app/hamzi/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function hamzi() {
-  return <>I am hamzi</>;
-}
